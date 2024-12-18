44968d2ca265447be64f2d35083023aef662d146
Mahesh <maheshkrishnan1242@gmail.com>
2024-12-18 14:34:11 +0530

    feat: Add responsiveness to HomeSection component

--- app/components/homesection/BottomSection.tsx
+++ app/components/homesection/BottomSection.tsx
@@ -4,7 +4,7 @@ const BottomSection = () => {
   return (
     <h1 className="text-center mt-2">
       Hi, I'm &nbsp;
-      <span className="font-stylish bg-gradientLight dark:bg-gradient dark:text-white rounded-lg px-4 py-2">
+      <span className="font-stylish bg-gradientLight text-nowrap dark:bg-gradient dark:text-white rounded-lg px-4 py-2">
         Mahesh krishnan
       </span>
       &nbsp; , a full-stack developer. I bring ideas to life by creating
