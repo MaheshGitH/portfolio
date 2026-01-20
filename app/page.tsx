8e7b5abbafaaf022a186e8d9105982f3b3eb8687
Mahesh Krishnan <maheshkrishnan1242@gmail.com>
2026-01-20 18:14:49 +0530

    chore: remove shadcn navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { StatefulButtonDemo } from "./components/StatefulButtonDemo";
-
 export default function Home() {
-  return (
-    <div className="">
-      <StatefulButtonDemo />
-
-      <Button className="text-green-400">Shadcn button</Button>
-    </div>
-  );
+  return <div className="h-screen"></div>;
 }
