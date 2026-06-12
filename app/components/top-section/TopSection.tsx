0dbb4584ed7bf14b8e893ed398a01307a7774b2d
Mahesh <maheshkrishnan1242@gmail.com>
2026-06-12 14:29:50 +0530

    remove wavy-background component due to high GPU usage

--- app/components/top-section/TopSection.tsx
+++ app/components/top-section/TopSection.tsx
@@ -1,4 +1,3 @@
-import { WavyBackground } from "@/components/ui/wavy-background";
 import ContactButton from "./ContactButton";
 import OpenToOpportunities from "./OpenToOpportunities";
 import Paragraph from "./Paragraph";
@@ -7,19 +6,17 @@ import ViewProjectButton from "./ViewProjectButton";
 
 const TopSection = () => {
   return (
-    <WavyBackground colors={["#9ca3af", "#23272c", "#14191f", "#0B0F14"]}>
-      <div className="h-screen flex flex-col gap-20 justify-center items-center">
-        <div className="flex flex-col items-center gap-8">
-          <OpenToOpportunities />
-          <Title />
-          <Paragraph />
-        </div>
-        <div className="flex gap-4">
-          <ViewProjectButton />
-          <ContactButton />
-        </div>
+    <div className="h-screen flex flex-col gap-20 justify-center items-center">
+      <div className="flex flex-col items-center gap-8">
+        <OpenToOpportunities />
+        <Title />
+        <Paragraph />
       </div>
-    </WavyBackground>
+      <div className="flex gap-4">
+        <ViewProjectButton />
+        <ContactButton />
+      </div>
+    </div>
   );
 };
 
